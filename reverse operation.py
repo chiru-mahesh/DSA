2fe8ec25a1edc870ffe6d53aba8be434ffe3aae9
chiru-mahesh <mahesh520004@gmail.com>
2026-04-20 12:11:24 +0530

    Add files via upload

---
+++ reverse operation.py
@@ -0,0 +1,37 @@
+#operations
+
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+head=None
+def insert(data):
+    global head
+    newnode=node(data)
+    newnode.next=head
+    head=newnode
+def reverse():
+    global head
+    prev=None
+    curr=head
+    while curr:
+        nextnode=curr.next
+        curr.next=prev
+        prev=curr
+        curr=nextnode
+    head=prev    
+def display():
+    temp=head
+    while temp:
+        print(temp.data,end="->")
+        temp=temp.next
+    print("None")
+
+n=int(input("Enter number of nodes:"))                         
+for _ in range(n):
+    val=int(input("Enter value:"))
+    insert(val)
+print("Inserted LinkedList:")
+display()
+reverse()
+display()    
